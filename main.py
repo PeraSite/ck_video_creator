e026568e5fa4044ed8e3e8a1fe5af919e6731e65
PeraSite <pretocki3@gmail.com>
2023-04-10 20:13:15 +0900

    feat: 인트로에 여러 음성을 넣을 수 있도록 수정, 순서.txt 삭제, 최종 영상은 명령어로 수행하도록 변경

--- main.py
+++ main.py
@@ -1,24 +1,44 @@
 import os
+import subprocess
+import shlex
 
 import ffmpeg
+
+input_data = [
+    "voices/음악방송_11회_녹음_1.m4a",
+    "voices/음악방송_11회_녹음_2.m4a",
+    "voices/음악방송_11회_녹음_3.m4a",
+    "musics/강하늘-24-자화상.mp3",
+    "musics/박효신-01-그 날 (Original Ver.).mp3",
+    "musics/안예은-01-8호 감방의 노래.mp3",
+    "voices/음악방송_11회_녹음_4.m4a",
+    "musics/김고은-06-그대 향한 나의 꿈.mp3",
+    "musics/정성화,조재윤,배정남,이현우-14-누가 죄인인가.mp3",
+    "voices/음악방송_11회_녹음_5.m4a",
+    "voices/음악방송_11회_녹음_6.m4a",
+    "voices/음악방송_11회_녹음_7.m4a",
+    "voices/음악방송_11회_녹음_8.m4a",
+]
+
+intro_data = {
+    "background_music": "musics/강하늘-24-자화상.mp3",
+    "voices": [0, 1, 2]
+}
 
 
 def main():
     file_list = read_input()
     voices = enhance_all_voices()
     intro_audio = create_intro_audio(voices, file_list)
-    final_audio = create_final_audio(intro_audio, voices, file_list)
 
-    # if image.png exists, create final video
+    final_audio = create_final_audio(intro_audio, voices, file_list)
+    (
+        final_audio.output("final_audio.mp3")
+        .overwrite_output()
+        .run()
+    )
     if os.path.exists("image.png"):
-        create_final_video(final_audio, "image.png", "final_video.mp4")
-    else:
-        print("No image.png found, creating final audio only...")
-        (
-            final_audio.output("final_audio.mp3")
-            .overwrite_output()
-            .run()
-        )
+        create_final_video()
 
     print("Done!")
 
@@ -26,13 +46,6 @@ def main():
 def read_input():
     print("Reading input...")
 
-    # 순서.txt 파일을 읽어서 파일 리스트를 만들기
-    input_data = []
-    with open("순서.txt", "r", encoding="utf-8") as f:
-        input_data.extend(f.readlines())
-    input_data = [file.strip() for file in input_data]
-
-    # 해당 파일이 있는지 검증
     for file in input_data:
         if not os.path.exists(file):
             raise FileNotFoundError(f"{file} does not exist!")
@@ -42,24 +55,24 @@ def read_input():
     return file_list
 
 
-def create_final_video(audio_stream, image_path: str, output_path: str):
+def create_final_video():
     print("Creating final video...")
 
-    # 1080p로 스케일링 후 mjpeg로 인코딩
-    (
-        ffmpeg
-        .output(audio_stream, ffmpeg.input(image_path), output_path, vf="scale=-1:1080", vcodec="mjpeg")
-        .overwrite_output()
-        .run()
-    )
+    command = "ffmpeg -r 1 -loop 1 -y -i image.png -i final_audio.mp3 -r 1 -pix_fmt yuv420p -vf scale=-1:1080 " \
+              "-shortest output.mp4"
+
+    subprocess.run(shlex.split(command), shell=True, check=True)
 
 
 def create_final_audio(intro_audio, voices, file_list: list[tuple[str, bool]]):
     print("Creating final audio...")
     audio_streams = [intro_audio]
 
-    # TODO: 하드코딩으로 초반 2개가 무조건 대사/음악인 부분 수정해야함
-    for file, is_voice in file_list[1:]:
+    # 인트로 음성 개수 제외
+    intro_size = len(intro_data["voices"])
+    print(f" - Skipping {intro_size} files...")
+
+    for file, is_voice in file_list[intro_size:]:
         print(f" - {file} - ({'Voice' if is_voice else 'Music'})")
         if is_voice:
             audio_streams.append(voices[file])
@@ -76,12 +89,14 @@ def create_intro_audio(voices, file_list: list[tuple[str, bool]]):
     print(f"Processing Intro...")
 
     # 처음으로 등장하는 음성과 음악 파일을 찾아서 처리
-    voice_path = next(file for file, is_voice in file_list if is_voice)
-    background_music_path = next(file for file, is_voice in file_list if not is_voice)
-    voice = voices[voice_path]
-
+    background_music_path = intro_data["background_music"]
     print(f" - Background music: {background_music_path}")
-    print(f" - Voice: {voice_path}")
+
+    print(f" - Voices:")
+    intro_voices = []
+    for i in intro_data["voices"]:
+        print(f"   - {file_list[i][0]}")
+        intro_voices.append(voices[file_list[i][0]])
 
     music_audio = (
         ffmpeg
@@ -91,13 +106,17 @@ def create_intro_audio(voices, file_list: list[tuple[str, bool]]):
     )
 
     voice_audio = (
-        voice
+        ffmpeg
+        .filter(intro_voices, "concat", n=len(intro_voices), v=0, a=1)
         .filter("adelay", "2000|2000")
     )
 
     return (
         ffmpeg
         .filter([voice_audio, music_audio], "amix", inputs=2, duration="first", normalize=0, dropout_transition=0)
+        .filter("areverse")
+        .filter("afade", duration=1)
+        .filter("areverse")
     )
 
 
